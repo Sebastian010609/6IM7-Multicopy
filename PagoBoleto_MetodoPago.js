cea9277661d2a2710985239fc02bf0a781fae296
Sebastian010609 <133049379+Sebastian010609@users.noreply.github.com>
2024-06-17 22:50:31 -0600

    Add files via upload

---
+++ PagoBoleto_MetodoPago.js
@@ -0,0 +1,68 @@
+function showPaymentModal(method) {
+    var modal = document.getElementById('paymentModal');
+    var form = document.getElementById('paymentForm');
+    modal.style.display = 'block';
+    form.onsubmit = function(event) {
+        event.preventDefault();
+        payWithMethod(method);
+    }
+}
+
+function closeModal() {
+    var modal = document.getElementById('paymentModal');
+    modal.style.display = 'none';
+}
+
+function payWithMethod(method) {
+    alert('Datos aceptados con éxito');
+    closeModal();
+    showNextButton();
+}
+
+function showNextButton() {
+    var nextButtonDiv = document.querySelector('.btnSiguiente');
+    nextButtonDiv.style.display = 'block'; 
+}
+
+
+function showNextButton() {
+    var nextButtonDiv = document.querySelector('.btnSiguiente');
+    nextButtonDiv.style.display = 'block'; // Muestra el botón "Siguiente"
+    nextButtonDiv.querySelector('.next-button').addEventListener('click', function() {
+        window.location.href = '../html/PagoBoleto_GenerarFanID.html'; // Cambia 'siguiente_pagina.html' por la URL a la que deseas redirigir
+    });
+}
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Array con las URLs de cada paso en el orden de los pasos
+    var stepUrls = [
+        "PagoBoleto_InformacionDelBoleto.html",
+        "PagoBoleto_SeleccionarAsiento.html",
+        "PagoBoleto_MetodoPago.html",
+        "PagoBoleto_GenerarFanID.html",
+        "PagoBoleto_FinalizarCompra.html"
+    ];
+
+    var steps = document.querySelectorAll('.step');
+    var currentStepIndex = findActiveStepIndex(steps);
+
+    steps.forEach(function(step, index) {
+        step.addEventListener('click', function() {
+            // Permitir la navegación solo si el paso objetivo es anterior al actual
+            if (index < currentStepIndex) {
+                window.location.href = stepUrls[index];
+            }
+        });
+    });
+
+    // Función para determinar el índice del paso actual basado en la clase 'active'
+    function findActiveStepIndex(steps) {
+        for (var i = 0; i < steps.length; i++) {
+            if (steps[i].classList.contains('active')) {
+                return i;
+            }
+        }
+        return -1; // Devuelve -1 si no se encuentra un paso activo
+    }
+});
---
+++ PagoBoleto_SeleccionarAsiento.js
@@ -0,0 +1,41 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var ticketInfos = document.querySelectorAll('.ticket-info');
+    ticketInfos.forEach(function(ticket) {
+        ticket.addEventListener('click', function() {
+            // Aquí solo necesitas poner la URL de la página a la que todos deben ser redirigidos
+            window.location.href = '../html/PagoBoleto_MetodoPago.html';
+        });
+    });
+});
+document.addEventListener('DOMContentLoaded', function() {
+    // Array con las URLs de cada paso en el orden de los pasos
+    var stepUrls = [
+        "PagoBoleto_InformacionDelBoleto.html",
+        "PagoBoleto_SeleccionarAsiento.html",
+        "PagoBoleto_MetodoPago.html",
+        "PagoBoleto_GenerarFanID.html",
+        "PagoBoleto_FinalizarCompra.html"
+    ];
+
+    var steps = document.querySelectorAll('.step');
+    var currentStepIndex = findActiveStepIndex(steps);
+
+    steps.forEach(function(step, index) {
+        step.addEventListener('click', function() {
+            // Permitir la navegación solo si el paso objetivo es anterior al actual
+            if (index < currentStepIndex) {
+                window.location.href = stepUrls[index];
+            }
+        });
+    });
+
+    // Función para determinar el índice del paso actual basado en la clase 'active'
+    function findActiveStepIndex(steps) {
+        for (var i = 0; i < steps.length; i++) {
+            if (steps[i].classList.contains('active')) {
+                return i;
+            }
+        }
+        return -1; // Devuelve -1 si no se encuentra un paso activo
+    }
+});
